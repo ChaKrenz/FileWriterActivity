6ef815ecbb8d30ff38d24e5bac4d26bd1aced692
Chance Krenzer <ckrenzer1@hwemail.com>
2024-09-06 14:33:38 -0700

    Edited SillyClass
    Changed class name and fixed error

--- SillyClass.java
+++ SillyClass.java
@@ -1,5 +1,5 @@
-public {
+public class SillyClass {
     public static void main (String[] args) {
-        System.out.println("My class is silly!")
+        System.out.println("My class is silly!");
     }
 }
