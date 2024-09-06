f97c395afe1542995c1197300ba6b64be05d7c60
ChaKrenz <ckrenzer1@hwemail.com>
2024-09-06 14:41:43 -0700

    Update SillyClass.java
    Added an extra special message for my friend.

--- SillyClass.java
+++ SillyClass.java
@@ -1,5 +1,6 @@
 public class SillyClass {
     public static void main (String[] args) {
         System.out.println("My class is silly!");
+        System.out.println("This is a silly class.");
     }
 }
