05fa05617cf5dd3370055a2aa81495097891c75b
Chance Krenzer <ckrenzer1@hwemail.com>
2024-09-11 11:28:34 -0700

    Homework 1.1
    Added a secret computer password file and created a .classified folder which printed a .confidential file with confedential.dat

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -12,39 +12,58 @@ public class MyFileWriter {
         String fileName4 = "example4.txt";
         String fileName5 = "example5.txt";
 
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
+        //Homework for September 10
+        String secretFileName = ".computerpassword.txt";
+        try (FileWriter writer = new FileWriter(secretFileName)) {
+            writer.write("Password: password123");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+        File repository = new File (".classified");
+        File fileName = new File(repository, "confidential.dat");
+        try (Writer writer = new FileWriter(fileName)) {
+            writer.write("Secret data");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+        
+
+//         // 1. Using FileWriter
+//         try (FileWriter writer = new FileWriter(fileName1)) {
+//             writer.write(data);
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         // 2. Using BufferedWriter
+//         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
+//             bufferedWriter.write(data);
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         // 3. Using FileOutputStream
+//         try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
+//             outputStream.write(data.getBytes());
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         // 4. Using BufferedOutputStream
+//         try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
+//             bufferedOutputStream.write(data.getBytes());
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         // 5. Using Files (java.nio.file)
+//         try {
+//             Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
